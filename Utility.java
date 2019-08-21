a2b83fa53d0d7b662b2a0688ef43dec9c1fcb580
Otec Fedor <fperesypin@gmail.com>
2019-08-21 17:08:17 +0300

    Добавлен поиск с помощью регулярных выражений

--- Utility.java
+++ Utility.java
@@ -1,22 +1,30 @@
 package Package;
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utility {
     public static void main(String[] args) throws Exception {
         int i = 0;
         FileReader file = new FileReader(args[args.length-1]);
         Scanner text = new Scanner(file);
+        Pattern pattern;
+        Matcher matcher;
         while (text.hasNextLine()) {
-            boolean success = false;
             String line = text.nextLine();
-            for (String word : args) {
-                if (line.toLowerCase().contains(word.toLowerCase())) {
-                    success = true;
+            String word;
+            for (int n = 0; n < args.length - 1; n++ ) {
+                word = args[n];
+               // System.out.println(word);
+                pattern = Pattern.compile(word, Pattern.CASE_INSENSITIVE);
+                matcher = pattern.matcher(line);
+                if(matcher.find()) {
+                    System.out.println((i++) + ". " + line);
                     break;
                 }
             }
-            if(success) System.out.println((i++) + ". " + line);
+
         }
         text.close();
         file.close();
